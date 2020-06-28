a0d809ca00209adeeb40c277b2c08453ae7530f2
Maximiliano Delgado <mdelgado@baufest.com>
2020-06-27 23:51:45 -0300

    sprite operator in react

--- react/src/js/components/app.js
+++ react/src/js/components/app.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data.json';
+import Loader from "./loader";
+
+console.log(data);
 
 function App() {
+    const [loaderList, setLoaderList] = useState([]);
+
+    const handleClick = () => {
+        setLoaderList(data.loaders);
+    }
+
     return (
         <div>
             que linda app
+            <ul>
+                {
+                    loaderList.map( item => <Loader {...item} key={item.id}/>)
+                }
+            </ul>
+            <button onClick={handleClick}>Mostrar</button>
         </div>
     )
 }
